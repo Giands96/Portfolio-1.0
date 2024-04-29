9d9908829d0edc091c3f9395707f667a000a16e3
Giands96 <dsg4857@gmail.com>
2024-04-29 17:09:45 -0500

    Retoques y eliminacion de paginas Blog y Contact

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,6 @@
 
+
+/* FUNCION SCROLL (VER PROYECTOS) */
 function scrollDown() {
     window.scrollTo({
         top: window.scrollY + 900,
@@ -6,3 +8,28 @@ function scrollDown() {
     });
 }
 
+/* LINK PROYECTO HOVER*/
+
+function cambio1(link) {
+    link.style.backgroundColor = "#6f6f6f";
+    link.style.transition = 'background-color .3s ease'
+
+}
+
+function cambio2(link) {
+    link.style.backgroundColor = "#363636";
+    link.style.transition = 'background-color .3s ease'
+
+}
+
+/* MOVIMIENTO TARJETAS HABILIDADES */
+
+function movimiento(card) {
+    card.style.transform = 'translateY(-30px)';
+
+}
+
+function movimiento2(card) {
+    card.style.transform = 'translateY(0px)';
+
+}
